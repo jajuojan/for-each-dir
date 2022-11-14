78fabd4e85f5709a7f63edc5304cbeaba7651fd0
Jani Ojanen <jajuojan@gmail.com>
2022-11-14 22:35:39 +0200

    fix: #25 Fix handling of quotes in commands

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,10 @@ struct Args {
     )]
     depth: Option<u8>,
 
-    #[clap(short, long, help = "Filter following directories")]
+    #[clap(short, long, default_value = "", help = "Filter following directories")]
     filter: String,
 
-    #[clap(short, long, help = "Ignore following directories")]
+    #[clap(short, long, default_value = "", help = "Ignore following directories")]
     ignore: String,
 }
 
@@ -35,14 +35,20 @@ fn main() {
     let args = Args::parse();
 
     let paths = fs::read_dir(".").unwrap();
+    let command = format_command(&args.command);
 
     for path in paths {
         let path_name = path.unwrap().path();
         let metadata = metadata(&path_name).unwrap();
         if metadata.is_dir() {
             println!("pushd \"{}\"", path_name.display());
-            println!("  {:?}", args.command);
+            println!("  {}", command);
             println!("popd\n");
         }
     }
+}
+
+/// Format the command to be executed
+fn format_command(raw_command: &String) -> String {
+    raw_command.replace("\\n", "\n  ")
 }
